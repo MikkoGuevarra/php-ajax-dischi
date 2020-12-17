66855eab2ebe7781816c72351bddb5d7466bda65
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-12-17 15:27:33 +0100

    int php vers

---
+++ php-version/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <link rel="stylesheet" href="../public/app.css">
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <h1>PHP Version</h1>
+        </header>
+        <main>
+            <section>
+                <div class="container">
+                    <div class="card-container">
+                        <div class="music-card">
+                            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg">
+                            <h3 class="album">disc.title</h3>
+                            <h4 class="artist">disc.author</h4>
+                            <p class="year">disc.year</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+    </body>
+</html>
